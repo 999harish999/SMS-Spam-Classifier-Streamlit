fcd46f70c6e0d75d360d665f161baa522b939437
Harish <999harish999@gmail.com>
2021-10-11 16:33:18 +0530

    Add files via upload

---
+++ nltk_download_utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 16:28:31 2021
+
+@author: harish
+"""
+
+# This file is for downloading nltk resorces 
+import nltk
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
